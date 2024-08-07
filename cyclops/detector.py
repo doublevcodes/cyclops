c7a1c95cf414e45ecff00c896c6c3d0f64872470
Pratibha Verma <vivaan.verma@gmail.com>
2024-08-07 17:20:39 +0100

    store as dictionary to encode as pickle dump

--- cyclops/detector.py
+++ cyclops/detector.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pickle
 
 import face_recognition
 
@@ -18,3 +19,17 @@ def encode_known_faces(
     for filepath in Path("train").glob("*/*"):
         name = filepath.parent.name
         image = face_recognition.load_image_file(filepath)
+
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+
+        name_encodings = {"names": names, "encodings": encodings}
+    
+    with encodings_location.open(mode="wb") as f:
+        pickle.dump(name_encodings, f)
+
+encode_known_faces()
