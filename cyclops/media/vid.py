c8f2f9cdd7338b9f2a03b26ac4b9b0be9a9656c1
Pratibha Verma <vivaan.verma@gmail.com>
2024-08-08 15:31:34 +0100

    add live face recognition in webcam video

--- cyclops/media/vid.py
+++ cyclops/media/vid.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import face_recognition
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
@@ -10,8 +11,27 @@ while True:
  
     if not ret:
         break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
+    
+    small_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+    
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+
+    for (top, right, bottom, left) in face_locations:
+    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Display the resulting image
+    cv.imshow('Video', frame)
+    
     if cv.waitKey(1) == ord('q'):
         break
  
